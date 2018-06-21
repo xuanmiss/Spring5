fc6286d41a0d8ce8afb1d24c7d9391a570c2bc7e
筱轩宝forever <1511836114@qq.com>
2018-06-21 13:29:27 +0800

    first commit

---
+++ src/main/java/com/miss/flux_demo/FluxDemoApplication.java
@@ -0,0 +1,12 @@
+package com.miss.flux_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FluxDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FluxDemoApplication.class, args);
+    }
+}
